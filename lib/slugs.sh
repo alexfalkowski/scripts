c8458012b72fc7d80105ebaaf22f95029b552001
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-04-15 18:21:16 +0200

    feat(ci): rotating the GitHub webhook secret for CircleCI GitHub OAuth project triggers (#139)
    https://support.circleci.com/hc/en-us/articles/49131607657243-Rotating-the-GitHub-webhook-secret-for-CircleCI-GitHub-OAuth-project-triggers

---
+++ lib/slugs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+readonly slugs=("github/alexfalkowski/bin" "gh/alexfalkowski/go-sync" "gh/alexfalkowski/go-signal" "gh/alexfalkowski/alexfalkowski.github.io" "gh/alexfalkowski/docker" "gh/alexfalkowski/bin" "gh/alexfalkowski/scripts" "gh/alexfalkowski/migrieren" "gh/alexfalkowski/standort" "gh/alexfalkowski/go-monolith" "gh/alexfalkowski/status" "gh/alexfalkowski/web" "gh/alexfalkowski/go-service-template" "gh/alexfalkowski/go-client-template" "gh/alexfalkowski/bezeichner" "gh/alexfalkowski/go-service" "gh/alexfalkowski/infraops" "gh/alexfalkowski/go-health" "gh/alexfalkowski/gocovmerge" "gh/alexfalkowski/nonnative" "gh/alexfalkowski/tausch")
